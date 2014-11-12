311c8b44f197f7b859ec495b48e4a8b08a5a43ab
Mclar053 <matrclark@gmail.com>
2014-11-12 21:55:52 +0000

    Tidy up of test

--- Testing/Testing.js
+++ Testing/Testing.js
@@ -2,16 +2,13 @@ var number = 0;
 	
 function addNumber(item)
 {
-	number+=Math.round(Math.random()*10);
-	document.getElementById("numbers").innerHTML = item+": " + number;
+	var number2=Math.round(Math.random()*10);
+	number+=number2;
+	document.getElementById("numbers").innerHTML = item+": " + number+" Start number: " + number2;
 }
 
 
 
 //Not using (Time Based)
 var seconds = 2642; // uptime in seconds
-var timer = setInterval(
-    function() {
-        seconds++;
-    }, 1000
-);
+var timer = setInterval(function() {seconds++;}, 1000);
