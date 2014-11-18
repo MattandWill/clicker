fb3eaf08fc4cce94edacb96c10777d429f52a3ab
Mclar053 <matrclark@gmail.com>
2014-11-18 12:33:17 +0000

    Changed things

--- scripts/mattTest.js
+++ scripts/mattTest.js
@@ -1,4 +1,4 @@
-var player = {
+var resources = {
     Timber:0,
     Pine:0,
     Oak:0,
@@ -11,13 +11,48 @@ var player = {
     Sandstone:0
 }
 
+var skills ={
+    
+}
+
 function changeHTML(){
-    document.getElementById("TimberNo").innerHTML="Timber: "+player.Timber;
-    document.getElementById("PineNo").innerHTML="Pine: "+player.Pine;
-    document.getElementById("OakNo").innerHTML="Oak: "+player.Oak;
+    document.getElementById("TimberNo").innerHTML="Timber: "+resources.Timber;
+    document.getElementById("PineNo").innerHTML="Pine: "+resources.Pine;
+    document.getElementById("OakNo").innerHTML="Oak: "+resources.Oak;
+    document.getElementById("SteelNo").innerHTML="Steel: "+resources.Steel;
+    document.getElementById("AlNo").innerHTML="Aluminium: "+resources.Aluminium;
+    document.getElementById("RGNo").innerHTML="Rusty Gate: "+resources.RustyGate;
+    document.getElementById("GraniteNo").innerHTML="Granite: "+resources.Granite;
+    document.getElementById("MarbleNo").innerHTML="Marble: "+resources.Marble;
+    document.getElementById("LimestoneNo").innerHTML="Limestone: "+resources.Limestone;
+    document.getElementById("SandstoneNo").innerHTML="Sandstone: "+resources.Sandstone;
 }
 
 function addStuff(){
-    player.Timber+=1;
+    resources.Timber+=1;
     changeHTML();
+}
+
+function addStuff2(){
+    resources.Timber+=1;
+    changeHTML();
+}
+
+function addStuff3(){
+    resources.Timber+=1;
+    changeHTML();
+}
+
+function addStuff4(){
+    resources.Timber+=1;
+    changeHTML();
+}
+
+function addStuff5(){
+    resources.Timber+=1;
+    changeHTML();
+}
+
+function checkSkillLvl(){
+    
 }
