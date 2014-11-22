84e62386d7a4a2c6d437f54b4106cfe276719b5f
williamchanter <williamchanter@gmail.com>
2014-11-22 01:57:28 +0000

    New way of doing things

--- scripts/mattTest.js
+++ scripts/mattTest.js
@@ -1,42 +1,85 @@
-var wood = {
-    timber:0,
-    pine:0,
-    oak:0 ,
-    mahogamy:0,
-    lahcenium:0
+var timber = new item("Wood",1);
+var pine = new item("Wood",10);
+var oak = new item("Wood",20);
+var mahogany = new item("Wood",30);
+var lahcenium = new item("Wood",50);
+
+var flint = new item("Stone",1);
+var limestone = new item("Stone",10);
+var marble = new item("Stone",20);
+var grannit = new item("Stone",30);
+var devlinese = new item("Stone",50);
+
+var steel = new item("metal",1);
+var alluminiunum = new item("metal",10);
+var silver = new item("metal",20);
+var gold = new item("metal",30);
+var marconium = new item("metal",50);
+
+var sapphire = new item("jewel",1);
+var ememrald = new item("jewel",10);
+var ruby = new item("jewel",20);
+var diamond = new item("jewel",30);
+var gilliesion = new item("jewel",50);
+
+var acrylic = new item("plastic",1);
+var pvc = new item("plastic",10);
+var polystyrene = new item("plastic",20);
+var polytetrafluoroethylene = new item("plastic",30);
+var katanium = new item("plastic",50);
+
+var mysticCow = new item("cookie",100);
+var fieniumBrinkious = new item("cookie",100);
+
+function item (type, requiredlvl) {
+    this.type = type;
+    this.requiredlvl = requiredlvl;
+    this.number = 0;
+    this.getInfo = function() {
+        return this.type + " " + this.requiredlvl;
+    }
 }
-var stone = {
-    flint:0,
-    limestone:0,
-    marble:0,
-    grannit:0,
-    devlinese:0
-}
-var metal = {
-    steel:0,
-    allumiunum:0,
-    silver:0,
-    gold:0,
-    marocnium:0 
-}
-var jewel = {
-    saphire:0,
-    ememrald:0,
-    ruby:0,
-    diamond:0,
-    gilliesion:0
-}
-var platics = {
-    acrylic:0,
-    pvc:0,
-    polystyrene:0,
-    polytetrafluoroethylene:0,
-    katanium:0
-}
-var cookies = {
-    mysticCow:0,
-    fieniumBrinkious:0
-}
+
+//var wood = {
+//    timber:0,
+//    pine:0,
+//    oak:0 ,
+//    mahogamy:0,
+//    lahcenium:0
+//}
+//var stone = {
+//    flint:0,
+//    limestone:0,
+//    marble:0,
+//    grannit:0,
+//    devlinese:0
+//}
+//var metal = {
+//    steel:0,
+//    allumiunum:0,
+//    silver:0,
+//    gold:0,
+//    marconium:0 
+//}
+//var jewel = {
+//    saphire:0,
+//    ememrald:0,
+//    ruby:0,
+//    diamond:0,
+//    gilliesion:0
+//}
+//var platics = {
+//    acrylic:0,
+//    pvc:0,
+//    polystyrene:0,
+//    polytetrafluoroethylene:0,
+//    katanium:0
+//}
+//var cookies = {
+//    mysticCow:0,
+//    fieniumBrinkious:0
+//}
+
 var skills ={
     gathering:1,
     exploration:1,
